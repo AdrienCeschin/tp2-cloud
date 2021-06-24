0789e31b35d1fb3499dc5aef6c40155d7c89dcfe
AdrienCeschin <adrien.ceschin.devops@gmail.com>
2021-06-24 16:15:19 +0200

    #CLOUD-AZURE - #TP-2: updates uris.

--- tp2.bash
+++ tp2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 ​
-templateUri=''
+templateUri='https://raw.githubusercontent.com/AdrienCeschin/tp2-cloud/master/azure-deploy-python-app.json'
 ressourceGroupName='tp2-adrienceschin'
 
 # Create a resource group.
